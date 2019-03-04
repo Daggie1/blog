afd82423b0004ed061eff93fa4bd1cd0e1ddf69e
Anonymous <daggyooks@gmail.com>
2019-03-04 13:03:32 +0300

    readme

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,21 +8,5 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public function testBasicTest()
-    {
-        $response = $this->post('/insert',[
-            'title'=>$title='just',
-            'description'=>$description='new desc',
 
-
-
-        ]);
-
-        $response->assertStatus(302);
-        $this->assertDatabaseHas('posts',[
-            'title'=>$title
-            ,'description'=>$description
-
-        ]);
-    }
 }
